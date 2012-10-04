4ab11985369bf90b873d4aeb8ccd9f1313b3efb3
yihuang <yi.codeplayer@gmail.com>
2012-10-04 17:15:51 +0800

    api change

--- Main.hs
+++ Main.hs
@@ -4,10 +4,10 @@ import Data.Conduit
 import qualified Data.Conduit.Binary as C
 import qualified Data.ByteString.Lazy.Char8 as L
 import Data.Aeson (encode)
-import Text.XML.ToJSON (xmlToJSON)
+import Text.XML.ToJSON (bsSourceToJSON)
 
 main :: IO ()
 main = do
     [name] <- getArgs
-    json <- runResourceT $ xmlToJSON (C.sourceFile name)
+    json <- runResourceT $ bsSourceToJSON (C.sourceFile name)
     L.putStrLn $ encode json
